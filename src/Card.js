863c3340f804e13f415b7139a7889a5e63df0b86
obleey <miha.oblishar@gmail.com>
2021-09-26 23:20:10 +0200

    added some destructuring and basic file cleaunp

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
+//import {robots} from "./robots";
 
-const Card = () => {
+const Card = ({name ,email, id}) => {
     return (
     <div className='bg-light-green dib pa3 br3 ma2 grow bw2 shadow-5'>
-        <img alt='robots' src='https://robohash.org/tests?200x200'/>
-        <div>
-            <h2> Jane doe</h2>
-            <p>email: miha.miha@gmail.com</p>
+        <img alt='robots' src={`https://robohash.org/${id}?200x200Ω`}/>
+        <div className='tc'>
+            <h2> {name}</h2>
+            <p>{email}</p>
         </div>
     </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import './index.css';
 import Card from './Card'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import {robots} from "./robots";
 //import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
       <div>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+          <Card id={robots[1].id} name={robots[2].name} email={robots[1].email}/>
+          <Card id={robots[2].id} name={robots[3].name} email={robots[2].email}/>
+          <Card id={robots[3].id} name={robots[4].name} email={robots[3].email}/>
       </div>
   </React.StrictMode>,
   document.getElementById('root')
