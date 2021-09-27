a85067a25db0f2d914114ddb0aae0d20c2cacf5e
in1309 <miha.oblisar@informatika.si>
2021-09-27 14:18:25 +0200

    Added Scroll.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import "./App.css"
+import Scroll from "./Scroll";
 
 class App extends Component
 {
@@ -17,7 +18,7 @@ class App extends Component
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({robots: users}));
+      .then(users => this.setState({robots: users}))
   }
 
   onSearchChange = (event) => {
@@ -28,14 +29,21 @@ class App extends Component
   const filteredRobots = this.state.robots.filter(robots => {
     return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 })
-
-    return (
-      <div className='tc'>
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+if (this.state.robots.length === 0)
+{
+  return <h1>Loading</h1>
+} else {
+  return (
+    <div className='tc'>
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchChange={this.onSearchChange}/>
+      <Scroll>
         <CardList robots={filteredRobots} />
-      </div>
-    );
+      </Scroll>
+    </div>
+  );
+}
+
   }
   ;
 }
